14c6aeccdba60b47f649eb321506f6a6fe45c252
shub-171 <shubhamagrawal14497@gmail.com>
2023-09-14 20:24:23 +0530

    ddd

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,11 +24,11 @@ export default function RootLayout({ children }) {
         <meta name="keywords" content="discount Amazon products, deals, savings, affordable shopping, Amazon discounts, online shopping, best deals, exclusive offers, budget shopping, Mega Sale Hub" />
         <link rel="canonical" href="https://www.megasalehub.shop" />
 
-        {/* <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/android-chrome-512x512.png" /> */}
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/android-chrome-512x512.png" />
 
 
         <meta property="og:title" content="Mega Sale Hub - Discounted Amazon Products, Deals, and Savings" />
