8e4523e86da08da50bc5c0e1819bcf7c952218b7
shub-171 <shubhamagrawal14497@gmail.com>
2023-09-24 20:59:00 +0530

    sss

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,8 @@ import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
-import ogImg from './assets/opengraph-image.png';
-import twImg from './assets/twitter-image.png';
+import ogImg from './opengraph-image.png';
+import twImg from './twitter-image.png';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,23 +25,30 @@ export default function RootLayout({ children }) {
         <meta name="keywords" content="discount Amazon products, deals, savings, affordable shopping, Amazon discounts, online shopping, best deals, exclusive offers, budget shopping, Mega Sale Hub" />
         <link rel="canonical" href="https://www.megasalehub.shop" />
 
-        <link rel="apple-touch-icon" sizes="57x57" href="./assets/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="./assets/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="./assets/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="./assets/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="./assets/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="./assets/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="./assets/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="./assets/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="./assets/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="./assets/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="./assets/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="./apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="./apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="./apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="./apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="./apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="./apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="./apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="./apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-icon-180x180.png" />
+
+        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-36x36.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-48x48.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-72x72.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-144x144.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-192x192.png" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="./favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
         <link rel="icon" type="image/x-icon" href='/favicon.ico'/>
-        <link rel="manifest" href="./assets/manifest.json" />
+        <link rel="manifest" href="./manifest.json" />
         <meta name="msapplication-TileColor" content="#fafafa" />
-        <meta name="msapplication-TileImage" content="./assets/ms-icon-144x144.png" />
+        <meta name="msapplication-TileImage" content="./ms-icon-144x144.png" />
         <meta name="theme-color" content="#fafafa" />
 
 
