e427f12ce2a4282ac7aee9f0509b4e46d848a663
shub-171 <shubhamagrawal14497@gmail.com>
2023-09-24 22:08:50 +0530

    s

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,18 +34,11 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" sizes="144x144" href="./apple-icon-144x144.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="./apple-icon-152x152.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="./apple-icon-180x180.png" />
-
-        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-36x36.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-48x48.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-72x72.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-144x144.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-192x192.png" />
-
         <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="./favicon-96x96.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
-        <link rel="icon" type="image/x-icon" href='/favicon.ico'/>
+        <link rel="manifest" href="./manifest.json" />
         <meta name="msapplication-TileColor" content="#fafafa" />
         <meta name="msapplication-TileImage" content="./ms-icon-144x144.png" />
         <meta name="theme-color" content="#fafafa" />
