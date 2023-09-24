7e369c11d8abb5fbd5bfc2e6c8872d84a09877a9
shub-171 <shubhamagrawal14497@gmail.com>
2023-09-24 21:20:43 +0530

    sdasd

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,9 +42,9 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-144x144.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="./android-icon-192x192.png" />
 
-        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="./favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.ico" />
+        <link rel="icon" type="image/png" sizes="96x96" href="./favicon-96x96.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.ico" />
         <link rel="icon" type="image/x-icon" href='/favicon.ico'/>
         <meta name="msapplication-TileColor" content="#fafafa" />
         <meta name="msapplication-TileImage" content="./ms-icon-144x144.png" />
